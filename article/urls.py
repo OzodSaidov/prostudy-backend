b03073a21c0c0f75e8eee256ec54c1310ab412a0
sherzodbek <9669470@gmail.com>
2021-07-05 19:07:18 +0500

    urls changed

--- article/urls.py
+++ article/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from article.views import AricleView
+# from article.views import AricleView
+from article.views import ArticleDetail, ArticleList, AuthorDetail, AuthorList
 
 urlpatterns = [
-    path('articles/', AricleView.as_view()),
-    path('articles/<int:pk>', AricleView.as_view()),
+    path('articles/', ArticleList.as_view()),
+    path('articles/<int:pk>', ArticleDetail.as_view()),
+    path('authors/', AuthorList.as_view()),
+    path('authors/<int:pk>', AuthorDetail.as_view()),
 ]
