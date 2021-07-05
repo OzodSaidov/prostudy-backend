a17f3a7784e819d39092ff5d1311a4238feb19d6
Ozod Saidov <ozodsaidov2@gmail.com>
2021-07-05 13:19:15 +0500

    fix model

--- user/models.py
+++ user/models.py
@@ -144,7 +144,7 @@ class Course(Base):
     href = models.CharField(max_length=200, null=True, verbose_name='uri')
     program_training = models.JSONField(default=dict)  # Программа обучения
     result = models.JSONField(default=dict)  # 100% результат
-    background = models.ImageField(upload_to='background/', validators=[validate_image_type])
+    background = models.ImageField(upload_to='background/', validators=[validate_image_type], null=True)
     menu = models.ForeignKey('Menu', on_delete=models.DO_NOTHING, related_name='courses')
 
     def __str__(self):
