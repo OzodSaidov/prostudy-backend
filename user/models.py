f4f7e82311f235d6e0b1a8c01e699afa05faa950
Ozod Saidov <ozodsaidov2@gmail.com>
2021-07-12 18:28:07 +0500

    fix Subscription model

--- user/models.py
+++ user/models.py
@@ -282,6 +282,7 @@ class SubscriptionRequest(Base):
     name = models.CharField(max_length=50)
     number_visitors = models.IntegerField()
     phone = models.CharField(max_length=13, validators=[validate_phone])
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='subscriptions', null=True)
     is_active = models.BooleanField(default=True)
 
     class Meta:
