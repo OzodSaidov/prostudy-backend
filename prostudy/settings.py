9926c53606b8648402bc6fb2b090b1f53e3b556f
ozod_saidov <ozodsaidov2@gmail.com>
2021-07-13 13:32:57 +0000

    added static_root to production

--- prostudy/settings.py
+++ prostudy/settings.py
@@ -166,6 +166,7 @@ STATIC_URL = '/static/'
 if DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
     STATICFILES_FINDERS = (
         'django.contrib.staticfiles.finders.FileSystemFinder',
         'django.contrib.staticfiles.finders.AppDirectoriesFinder',
